12bd5f24741847f2227951d6b968a5f11a06b7bf
Rinezki <ville.salmi98@gmail.com>
2025-12-11 17:48:13 +0200

    test

---
+++ test.py
@@ -0,0 +1,10 @@
+from test import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()   # Käynnistää Chrome-selaimen
+
+driver.get("https://google.com")   # Avaa Googlen
+
+print(driver.title)   # Tulostaa välilehden nimen
+
+driver.quit()   # Sulkee selaimen
